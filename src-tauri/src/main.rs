430ebda22483a4782e706c2c212e8e33999f88f3
Dominik Nakamura <dnaka91@gmail.com>
2022-02-14 15:53:44 +0900

    Add common menu items

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -5,11 +5,39 @@
 
 use tauri::{
     api::process::{Command, CommandEvent, TerminatedPayload},
-    Manager, Window,
+    Manager, Menu, MenuEntry, MenuItem, Submenu, Window,
 };
 
 fn main() {
     tauri::Builder::default()
+        .menu(Menu::with_items([
+            MenuEntry::Submenu(Submenu::new(
+                "memebox",
+                Menu::with_items([
+                    MenuItem::About("memebox".to_owned()).into(),
+                    MenuItem::Separator.into(),
+                    MenuItem::Services.into(),
+                    MenuItem::Separator.into(),
+                    MenuItem::Hide.into(),
+                    MenuItem::HideOthers.into(),
+                    MenuItem::ShowAll.into(),
+                    MenuItem::Separator.into(),
+                    MenuItem::Quit.into(),
+                ]),
+            )),
+            MenuEntry::Submenu(Submenu::new(
+                "File",
+                Menu::with_items([MenuItem::CloseWindow.into()]),
+            )),
+            MenuEntry::Submenu(Submenu::new(
+                "View",
+                Menu::with_items([MenuItem::EnterFullScreen.into()]),
+            )),
+            MenuEntry::Submenu(Submenu::new(
+                "Window",
+                Menu::with_items([MenuItem::Minimize.into(), MenuItem::Zoom.into()]),
+            )),
+        ]))
         .setup(|app| {
             let window = app.get_window("main").unwrap();
             tauri::async_runtime::spawn(async move { run_sidecar(window).await });
