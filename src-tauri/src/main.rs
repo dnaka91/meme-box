2a8e7dc9b76646a4c8a8694bde47a912c2c5d891
Dominik Nakamura <dnaka91@gmail.com>
2022-09-27 19:20:07 +0900

    Update to latest Tauri release

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,8 +9,8 @@ use clap::Parser;
 use serde::Deserialize;
 use tauri::{
     api::process::{Command, CommandEvent, TerminatedPayload},
-    CustomMenuItem, Manager, Menu, MenuEntry, MenuItem, Submenu, SystemTray, SystemTrayEvent,
-    SystemTrayMenu, SystemTrayMenuItem, Window,
+    CustomMenuItem, Manager, Menu, SystemTray, SystemTrayEvent, SystemTrayMenu, SystemTrayMenuItem,
+    Window,
 };
 
 #[derive(Parser)]
@@ -29,6 +29,7 @@ struct Opt {
 
 fn main() {
     let opt = Opt::parse();
+    let context = tauri::generate_context!();
 
     let tray = SystemTray::new().with_menu(
         SystemTrayMenu::new()
@@ -39,34 +40,7 @@ fn main() {
     );
 
     tauri::Builder::default()
-        .menu(Menu::with_items([
-            MenuEntry::Submenu(Submenu::new(
-                "memebox",
-                Menu::with_items([
-                    MenuItem::About("memebox".to_owned()).into(),
-                    MenuItem::Separator.into(),
-                    MenuItem::Services.into(),
-                    MenuItem::Separator.into(),
-                    MenuItem::Hide.into(),
-                    MenuItem::HideOthers.into(),
-                    MenuItem::ShowAll.into(),
-                    MenuItem::Separator.into(),
-                    MenuItem::Quit.into(),
-                ]),
-            )),
-            MenuEntry::Submenu(Submenu::new(
-                "File",
-                Menu::with_items([MenuItem::CloseWindow.into()]),
-            )),
-            MenuEntry::Submenu(Submenu::new(
-                "View",
-                Menu::with_items([MenuItem::EnterFullScreen.into()]),
-            )),
-            MenuEntry::Submenu(Submenu::new(
-                "Window",
-                Menu::with_items([MenuItem::Minimize.into(), MenuItem::Zoom.into()]),
-            )),
-        ]))
+        .menu(Menu::os_default(&context.package_info().name))
         .system_tray(tray)
         .on_system_tray_event(|app, event| {
             if let SystemTrayEvent::MenuItemClick { id, .. } = event {
@@ -92,7 +66,7 @@ fn main() {
 
             Ok(())
         })
-        .run(tauri::generate_context!())
+        .run(context)
         .expect("error while running tauri application");
 }
 
